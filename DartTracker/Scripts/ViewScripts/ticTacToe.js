c21c6cabf170f5e77d50ec7a008c76820f4aabc8
Anthony DiPerna <dipernaa@msoe.edu>
2014-09-05 11:53:12 -0500

    Added tic tac toe, still in early stages

---
+++ DartTracker/Scripts/ViewScripts/ticTacToe.js
@@ -0,0 +1,34 @@
+﻿$().ready(function () {
+    $(".gameTable").css({ "height" : $(".gameTable").width() + "px" });
+
+    var TicTacToeLine = function (array) {
+        var self = this;
+        self.boxes = ko.observableArray();
+
+        var i;
+        for (i = 0; i < array.length; i = i + 1) {
+            self.boxes.push(new TicTacToeBox(array[i]));
+        }
+    }
+
+    var TicTacToeBox = function (number) {
+        var self = this;
+        self.imageSrc = ko.observable("../Assets/blank.png");
+        self.number = ko.observable(number);
+    }
+
+    var GameViewModel = function () {
+        var self = this;
+
+        self.ticTacToeNumbers = ko.observableArray([
+            new TicTacToeLine(["12", "20", "18"]),
+            new TicTacToeLine(["11", "B", "6"]),
+            new TicTacToeLine(["7", "3", "2"])]);
+
+        self.handleClick = function (line) {
+            line.imageSrc("../Assets/circle-white.png");
+        }
+    }
+
+    ko.applyBindings(new GameViewModel());
+})
